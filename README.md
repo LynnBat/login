4caec15f72475a4aa87acb0905a3028a23d6a570
Lynn Sokolova <ele@sprks.eu>
2021-05-01 22:38:43 +0200

    small changes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## Instalation
+## Installation
 1. `git clone git@github.com:LynnBat/login.git`
 2. `bundle install`
 3. Setup all necessary environment variables in `.env`
@@ -8,7 +8,14 @@ All variables from `.env.sample` have to be filled with proper values in `.env`
 
 `APP_HOST` - URL
 
-`BROWSER` - with a current setup of spec_helper.rb, this variable can be only `chrome` for tests on Chrome browser
+`BROWSER` - with a current setup of spec_helper.rb, this variable can be only `chrome` for tests on Google Chrome browser
 
 ## Running Tests
-`rspec <dir>`
+In order to run all specs 
+`rspec`
+
+In order to run exact spec
+`rspec <dir>/<file>`
+
+In order to run exact test in the spec
+`rspec <dir>/<file>:<line_number>`
--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -4,17 +4,24 @@ RSpec.feature 'Login page' do
   let(:user)   { User.new }
   let(:router) { Router.new }
 
-  before { create_new_user }
-
   scenario 'can successfully create an account' do
+    visit router.signup_path
+    fill_in 'Full name', with: user.name
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Sign Up'
+
     expect(page).not_to have_css('#ls-user-signup')
     find('.redesign-container', text: 'My Stuff').click
     expect(page).to have_css('#sign-out')
   end
 
   scenario 'can successfully log in' do
+    create_new_user
     logout
-    login
+    visit router.login_path
+    fill_inputs
+    click_button 'Sign In'
 
     expect(page).not_to have_css('#ls-user-signup')
     find('.redesign-container', text: 'My Stuff').click
